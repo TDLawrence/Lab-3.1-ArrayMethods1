dcf62c643c497a02b44a1d1750efff87624bfcb3
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-01-18 09:23:30 -0500

    Added InsertionSort;began selctionSort

--- ArrayMethods1 Lab 3.1 Tyson Lawrence period 2/src/ArrayMethods1.java
+++ ArrayMethods1 Lab 3.1 Tyson Lawrence period 2/src/ArrayMethods1.java
@@ -2,7 +2,17 @@
 public class ArrayMethods1 {
 
 	public static void insertionSort(int[]list1)
-	{
+	{int swap=0;
+	 while(swap>0)
+	 {for(int n=list1.length-1;n>=0;n--)
+	  {if (list1[n]<list1[n-1])
+	    swap(list1,n,n-1);
+	    swap++;
+	   if(list1[n]==list1[n-1])
+		 n=n-1;	 
+	  }
+		 
+	 }
 		
 	}
 	public static void bubbleSort(String[]list1)
@@ -21,7 +31,21 @@ public class ArrayMethods1 {
 	}
 	public static void selectionSort(double[]list1)
 	{
-		
+	 double curmin=0;
+	 int minIndex=0;
+	  for(int n=0;n<list1.length;n++)
+		  {curmin=list1[n];
+	      minIndex=n;
+	        for(int p=n+1;p<list1.length;p++)
+		    {if (curmin>list1[p])
+		    curmin=list1[p];
+		     minIndex=p;
+		    }
+	     if(
+		 swap(list1,minIndex,n);
+	     }
+	 
+	 
 	}
 	public static void swap(int[] arr,int index1,int index2)
 	{int y=arr[index1];
